69f50262b4e7553ff8e149b56137f6d0d4eb7aea
RGSntj <mariadefatima071965@gmail.com>
2023-04-07 20:41:43 -0300

    create screens types

---
+++ src/types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      detail: { name: string };
+      favorites: undefined;
+      search: undefined;
+    }
+  }
+}
