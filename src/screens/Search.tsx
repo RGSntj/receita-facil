cec4071b55d299f56126d658714bc6cfd3b784cd
RGSntj <mariadefatima071965@gmail.com>
2023-04-09 16:51:54 -0300

    :tada: finalished 'Search' screen

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+
+import { useRoute } from "@react-navigation/native";
+import { api } from "../services/api";
+import { FoodList, FoodsProps } from "../components/FoodList";
+
+interface Params {
+  name: string;
+}
 
 export function Search() {
+  const [receipes, setReceipes] = useState<FoodsProps[]>([]);
+
+  const route = useRoute();
+  const { name } = route.params as Params;
+
+  useEffect(() => {
+    async function fetchReceipes() {
+      const response = await api.get(`/foods?name_like=${name}`);
+      setReceipes(response.data);
+    }
+
+    fetchReceipes();
+  }, [name]);
+
   return (
-    <View>
-      <Text>SEARCH SCREEN</Text>
+    <View className="flex-1 bg-background pt-2 px-2 pb-2">
+      <FlatList
+        data={receipes}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <FoodList data={item} />}
+        ListEmptyComponent={() => (
+          <Text className="color-white font-bold text-xl">
+            Não encotramos o que está buscando...
+          </Text>
+        )}
+      />
     </View>
   );
 }
